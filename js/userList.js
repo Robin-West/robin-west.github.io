f2505ce62f2e1779ee9e076aed8b63b956144eca
pzdouglas <pdouglas@zebra.com>
2022-09-20 13:03:59 -0500

    Updates to display/navigation using mock data

--- js/userList.js
+++ js/userList.js
@@ -1,19 +1,22 @@
 const getUsers = async () => {
-    value = getCookie("token");
+//     value = getCookie("token");
+//     let list = "";
+//     const response = await fetch('https://stage-api.zebra.com/v2/phoenixDemoApp/api/users/?pageSize=30&tenantId=api_demo_app', {
+//       method: 'GET',
+//       headers: {
+//         'accept': 'application/json',
+//         'authorization': value
+//       }
+//     });
+//     let resp = await response;
+//     if (resp.ok){
+//         const myJson = resp.json(); //extract JSON from the http response
+//         list = resp.text;
+//     }
+//     else
+//         list = "Error: " + resp.statusText;
+//     return list;
+
     let list = "";
-    const response = await fetch('https://stage-api.zebra.com/v2/phoenixDemoApp/api/users/?pageSize=30&tenantId=api_demo_app', {
-      method: 'GET',
-      headers: {
-        'accept': 'application/json',
-        'authorization': value
-      }
-    });
-    let resp = await response;
-    if (resp.ok){
-        const myJson = resp.json(); //extract JSON from the http response
-        list = resp.text;
-    }
-    else
-        list = "Error: " + resp.statusText;
     return list;
   }
