bf107d60e49a108d2f4dc6402776148906d1257e
Tim Quinnell <timquinnell@users.noreply.github.com>
2022-09-21 13:46:56 -0500

    Changing login URI per SSO changes.

--- js/login.js
+++ js/login.js
@@ -38,7 +38,8 @@ function deleteCookie(cname)
 }
 
 function login() {
-  window.location.replace("https://uat-ssppcorep.pp.zebra.com/api/identity/token/api_demo_app?redirectUri=" + window.location.href);
+//   window.location.replace("https://uat-ssppcorep.pp.zebra.com/api/identity/token/api_demo_app?redirectUri=" + window.location.href);
+    window.location.replace("https://uat-ssppcorep.pp.zebra.com/identity/login/api_demo_app?redirectUri=" + window.location.href);
 }
 
 function refreshToken(callback) {
